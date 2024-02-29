1ae3476b4e0840b51c30faef54ca735f2b9b4ada
Stuart George <stuart@accentdesign.co.uk>
2024-02-29 18:56:03 +0000

    remove code defaults

--- internal/config/config.go
+++ internal/config/config.go
@@ -48,14 +48,13 @@ type Config struct {
 }
 
 // NewConfigFromPath creates and validates a new Config from a .toml file.
-// Uses a set of default values from DefaultConfig.
-// If environment variables are set, they will override the values from the .toml file
-// and the default values. The environment variables must be prefixed with the
+// If environment variables are set, they will override the values from the .toml file.
+// The environment variables must be prefixed with the
 // name of the configuration structure in uppercase, and the keys must be separated
 // by underscores. For example, to override the `port` value in the `server` structure,
 // the environment variable must be `SERVER_PORT`.
 func NewConfigFromPath(path string) (*Config, error) {
-	config := DefaultConfig()
+	config := &Config{}
 
 	viper.SetConfigFile(path)
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
@@ -68,44 +67,6 @@ func NewConfigFromPath(path string) (*Config, error) {
 		return nil, err
 	}
 	return config, nil
-}
-
-// DefaultConfig creates a Config instance populated with default values.
-func DefaultConfig() *Config {
-	return &Config{
-		Server: ServerConfig{
-			Port: 80,
-			Mode: ServerModeRelease,
-		},
-		Database: DatabaseConfig{
-			Host:     "localhost",
-			Port:     5432,
-			User:     "postgres",
-			Password: "password",
-			Db:       "db",
-			SslMode:  SslModeDisable,
-		},
-		Security: SecurityConfig{
-			AllowedHosts:          []string{},
-			StsSeconds:            86400 * 365,
-			StsIncludeSubdomains:  true,
-			FrameDeny:             true,
-			ContentTypeNosniff:    true,
-			BrowserXSSFilter:      true,
-			ContentSecurityPolicy: "default-src 'self'",
-			CsrfSecret:            "some_secret_key",
-		},
-		Session: SessionConfig{
-			Key:      "13d45bf0a822b832cc8886fa41ce4ced30584189bad02ec8ce552ace0d1ae8b1",
-			EncKey:   "2bb61a68ac3dec4f7c25efb062f4ae3b",
-			Path:     "/",
-			Domain:   "",
-			MaxAge:   86400 * 30,
-			Secure:   false,
-			HttpOnly: true,
-			SameSite: http.SameSiteLaxMode,
-		},
-	}
 }
 
 // ServerConfig represents the server configuration.
